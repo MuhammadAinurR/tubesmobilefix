3b61d8fd489da139586ec73245bf5d1429699d44
Administrator <ainurmoh@gmail.com>
2020-05-25 19:19:56 +0700

    tubes mobile

---
+++ app/src/main/java/com/example/toserba/loginapp.java
@@ -0,0 +1,14 @@
+package com.example.toserba;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class loginapp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loginapp);
+    }
+}
